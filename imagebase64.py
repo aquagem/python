69aa2425ccdbd615f483111ed116e5576c861371
gmaroli <gautham.maroli@gmail.com>
2018-04-23 15:40:11 +1000

    Update imagebase64.py

--- imagebase64.py
+++ imagebase64.py
@@ -1,6 +1,9 @@
 import base64
 
 def getImageBase64(image_path):
+  '''
+  This function is used to get the base64 value of an image which can be used to embed the image
+  '''
   with open(image_path, "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
     image_file.close
